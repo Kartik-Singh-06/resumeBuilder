352ad6e08d70d8c96ff408ba6278f817099d787b
Kartik Singh <tkartik0661@gmail.com>
2025-01-14 17:05:43 +0530

    Develop create Resume page

---
+++ src/Pages/AddResume.jsx
@@ -0,0 +1,92 @@
+import { Loader2, PlusSquare } from "lucide-react";
+import React, { useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/Components/ui/button";
+import { Input } from "@/components/ui/input";
+import { v4 as uuidv4 } from "uuid";
+import GlobalApi from "../../Service/GlobalApi";
+import { useUser } from "@clerk/clerk-react";
+
+const AddResume = () => {
+  const [toggleDialog, setToggleDialog] = useState(false);
+  const [resumeTitle, setresumeTitle] = useState("");
+  const { user } = useUser();
+  const [loading, setLoading] = useState(false);
+
+  const onCreate = async () => {
+    setLoading(true);
+    const uID = uuidv4();
+    const data = {
+      data: {
+        title: resumeTitle,
+        resumeId: uID,
+        userEmail: user?.primaryEmailAddress?.emailAddress,
+        userName: user?.fullName,
+      },
+    };
+
+    try {
+      const response = await GlobalApi.CreateNewResume(data);
+      console.log("Resume created successfully:", response.data);
+      setLoading(false);
+      // setToggleDialog(false); // Close dialog after success
+    } catch (error) {
+      console.error("Error creating resume:", error.response?.data || error.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <div
+        className="w-[12vw] h-[30vh] border-dashed border-2 border-[#b0afaf] rounded-md cursor-pointer flex justify-center items-center bg-[#e8e8e8] hover:scale-105 transition-all hover:shadow-lg"
+        onClick={() => setToggleDialog(true)}
+      >
+        <PlusSquare />
+      </div>
+
+      <AlertDialog open={toggleDialog}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Create New Resume</AlertDialogTitle>
+            <AlertDialogDescription>
+              <p className="mb-2">Add title for your resume</p>
+              <Input
+                type="text"
+                placeholder="Ex. Frontend Developer..."
+                onChange={(e) => setresumeTitle(e.target.value)}
+              />
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <Button
+              onClick={() => setToggleDialog(false)}
+              className="bg-[#007AFF] hover:bg-[#312ECB]"
+            >
+              Cancel
+            </Button>
+            <Button
+              disabled={!resumeTitle || loading}
+              onClick={onCreate}
+              className="bg-[#007AFF] hover:bg-[#312ECB]"
+            >
+              {loading ? <Loader2 className="animate-spin" /> : "Create"}
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+};
+
+export default AddResume;
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import AddResume from './AddResume'
 
 const Dashboard = () => {
   return (
-    <div >Dashboard</div>
+    <div className='p-10 md:px-20 lg:px-32'>
+      <h2 className='text-3xl font-bold'>My Resume</h2>
+      <p className='w-[50vw] mt-1 text-sm text-[#575757] '>Create your resume effortlessly to showcase your skills, experience, and achievements. Start building a professional resume that stands out and helps you land your dream job.</p>
+
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mt-12 '>
+        <AddResume/>
+      </div>
+    </div>
   )
 }
 
